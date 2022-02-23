1a3b0150e90e38fe82bc5e43a02db227239dc8a9
Pablo González Rubio <pjgr2000@gmail.com>
2022-02-23 18:21:56 +0100

    Fixed formatting of metrics.

--- monitor-agent/core/models/metricModel.py
+++ monitor-agent/core/models/metricModel.py
@@ -31,7 +31,7 @@ class MetricDynamic:
     def __init__(self):
         self.cpu_freq = psutil.cpu_freq()._asdict()
         self.cpu_percent = psutil.cpu_percent()
-        self.ram = _ram_data()
+        self.ram = psutil.virtual_memory()._asdict()
         self.network_current_in, self.network_current_out = _cur_network_traffic()
         self.network_total_in, self.network_total_out = _total_network_traffic()
         # self.connections = {}
@@ -46,27 +46,6 @@ class MetricDynamic:
         # self.process = {p.pid: p.info for p in psutil.process_iter(['name', 'username'])}
         self.disk_percent = _disk_percent()
         self.uptime = _uptime()
-
-
-def _get_size(bytes: int, suffix="B"):
-    """Scale bytes to its proper format.
-    e.g:
-        1253656 => '1.20MB'
-        1253656678 => '1.17GB'
-
-    Args:
-        bytes (int): Numeric size to format
-        suffix (str): Format of introduced size: 'B', 'K', 'M', 'G', 'T', 'P'
-
-    Returns:
-        string: Formatted size
-
-    """
-    factor = 1024
-    for unit in ["", "K", "M", "G", "T", "P"]:
-        if bytes < factor:
-            return f"{bytes:.2f}{unit}{suffix}"
-        bytes /= factor
 
 
 def _format_timestamp(date: float):
@@ -113,33 +92,18 @@ def _cur_network_traffic(interval=1):
     current_in = 0 if net1_in > net2_in else net2_in - net1_in
     current_out = 0 if net1_out > net2_out else net2_out - net1_out
 
-    # Final Variables
-    # Traffic in Megabytes
-    network_current_in = _get_size(round(current_in, 2))
-    network_current_out = _get_size(round(current_out, 2))
-
-    return network_current_in, network_current_out
+    return current_in, current_out
 
 
 def _total_network_traffic():
     # get IO statistics since boot
     net_io = psutil.net_io_counters()
-    network_total_out = _get_size(net_io.bytes_sent)
-    network_total_in = _get_size(net_io.bytes_recv)
-    return network_total_in, network_total_out
+    return net_io.bytes_recv, net_io.bytes_sent
 
 
 def _boot_date():
     boot_time_timestamp = psutil.boot_time()
     return _format_timestamp(boot_time_timestamp)
-
-
-def _ram_data():
-    ram = psutil.virtual_memory()._asdict()
-    for key, value in ram.items():
-        if key != "percent":
-            ram[key] = _get_size(value)
-    return ram
 
 
 def _ip_addresses():
@@ -169,15 +133,6 @@ def _disk_list():
         try:
             disk_location = disk.device
             disk_list[disk_location] = psutil.disk_usage(disk_location)._asdict()
-            disk_list[disk_location]["free"] = _get_size(
-                disk_list[disk_location]["free"]
-            )
-            disk_list[disk_location]["total"] = _get_size(
-                disk_list[disk_location]["total"]
-            )
-            disk_list[disk_location]["used"] = _get_size(
-                disk_list[disk_location]["used"]
-            )
         except PermissionError as msg:
             continue
     return disk_list
