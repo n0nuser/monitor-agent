5ea3e725e2c06e62b18a642677f62c84054f0fc4
Pablo González Rubio <pjgr2000@gmail.com>
2022-02-26 15:13:42 +0100

    Implemented alert manager.
    Need to check if it's the best way to store the thresholds.

--- monitor_agent/main.py
+++ monitor_agent/main.py
@@ -2,6 +2,7 @@ import sys
 import json
 import uvicorn
 import logging
+import requests
 from fastapi import FastAPI, UploadFile, File
 from fastapi_utils.tasks import repeat_every
 from .settings import Settings
@@ -20,14 +21,25 @@ api = FastAPI()
 endpoints = {
     "root": "/",
     "command": "/command",
-    "metrics_endpoint": "/metrics",
+    "metrics": "/metrics",
     "settings": "/settings",
+    "thresholds": "/thresholds",
+}
+
+thresholds = {
+    "cpu_percent": 50,
+    "ram_percent": 30,
 }
 
 # GET
 @api.get(endpoints["root"])
 async def root():
-    return {"message": "Hello World"}
+    return {"endpoints": endpoints}
+
+
+@api.get(endpoints["thresholds"])
+async def root():
+    return {"thresholds": thresholds}
 
 
 @api.get(endpoints["settings"])
@@ -39,7 +51,7 @@ async def mod_settings():
 
 if config_file.metric_endpoint:
 
-    @api.get(endpoints["metrics_endpoint"])
+    @api.get(endpoints["metrics"])
     async def metrics_endpoint():
         elapsed_time, data = send_metrics_adapter([static, dynamic])
         return {"data": data, "elapsed_time": elapsed_time}
@@ -61,6 +73,18 @@ async def mod_settings(settings: UploadFile):
     return config_file.write_settings(data)
 
 
+@api.post(endpoints["thresholds"])
+async def mod_settings(
+    cpu_percent: float = thresholds["cpu_percent"],
+    ram_percent: float = thresholds["ram_percent"],
+):
+    # if token:
+    #     blablabla
+    thresholds["cpu_percent"] = cpu_percent
+    thresholds["ram_percent"] = ram_percent
+    return {"thresholds": thresholds}
+
+
 @api.on_event("startup")
 @repeat_every(seconds=config_file.post_interval, logger=logger, wait_first=True)
 def periodic():
@@ -73,8 +97,17 @@ def periodic():
         elapsed_time=elapsed_time,
         data=data,
         file_enabled=config_file.metric_enable_file,
-        file_path=config_file.abs_file_path,
+        file_path=config_file.metric_file,
     )
+    alert = {}
+    if data["cpu_percent"] >= thresholds["cpu_percent"]:
+        alert["cpu_percent"] = data["cpu_percent"]
+    if data["ram"]["percent"] >= thresholds["ram_percent"]:
+        alert["ram_percent"] = data["ram"]["percent"]
+    if data["process"]:
+        alert["processes"] = data["process"]
+    if alert:
+        r = requests.post(config_file.alert_url, json={"alert": alert})
 
 
 def start():
