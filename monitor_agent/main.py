084325b80519a151fdd522ca75782998d0a8db23
Pablo González Rubio <pjgr2000@gmail.com>
2022-03-26 12:52:49 +0100

    Devel (#25)
    * Changed Pydantic for JSON config files.
    
    * Fixed poetry entry-point.
    
    It threw the error 'EntryPoint must be in 'name=module:attrs [extras]' format'. Fixed it changing the '-' in 'monitor-agent' for a '_'.
    
    * Implemented receiving settings as a JSON ->FILE<- (not text).
    
    Need to fix testing of data types when a JSON is received
    
    * Reformatted code of 'settings' module
    
    * Implemented alert manager.
    
    Need to check if it's the best way to store the thresholds.
    
    * Fix #24.
    
    * SECURITY: Removed GET Settings.
    
    * Remote Work
    
    * Settings reformmated and FINALLY FIXED. Removed Thresholds POST
    
    Thresholds will now be passed through the config file instead of saving it as a dictionary.
    
    * Bump Dependency Versions

--- monitor_agent/main.py
+++ monitor_agent/main.py
@@ -4,15 +4,17 @@ import json
 import uvicorn
 import logging
 import requests
-from urllib3.exceptions import MaxRetryError, NewConnectionError
-from fastapi import FastAPI, UploadFile, File
-from fastapi_utils.tasks import repeat_every
-from monitor_agent.core.helper import save2log
 from .settings import Settings
-from .core.metricFunctions import send_metrics, send_metrics_adapter, static, dynamic
 from .core.command import Command
+from fastapi import FastAPI, UploadFile
+from fastapi_utils.tasks import repeat_every
+from .core.metricFunctions import send_metrics, send_metrics_adapter, static, dynamic
 
-config_file = Settings()
+try:
+    CONFIG = Settings()
+except json.decoder.JSONDecodeError as msg:
+    print('Error in "settings.json".', msg, file=sys.stderr)
+    exit()
 
 logger = logging.getLogger(__name__)
 api = FastAPI()
@@ -25,10 +27,6 @@ endpoints = {
     "thresholds": "/thresholds",
 }
 
-thresholds_dict = {
-    "cpu_percent": 50,
-    "ram_percent": 30,
-}
 
 # GET
 @api.get(endpoints["root"])
@@ -38,10 +36,10 @@ async def root():
 
 @api.get(endpoints["thresholds"])
 async def thresholds():
-    return {"thresholds": thresholds_dict}
+    return {"thresholds": CONFIG.thresholds.__dict__}
 
 
-if config_file.metric_endpoint:
+if CONFIG.metrics.get_endpoint:
 
     @api.get(endpoints["metrics"])
     async def metrics_endpoint():
@@ -52,56 +50,32 @@ if config_file.metric_endpoint:
 # POST
 @api.post(endpoints["command"])
 async def command(command: str, timeout: int):
-    # if token:
-    #     blablabla
     return Command(command, timeout).__dict__
 
 
 @api.post(endpoints["settings"])
 async def mod_settings(settings: UploadFile):
-    # if token:
-    #     blablabla
+    global CONFIG
     data: str = settings.file.read().decode()
-    return config_file.write_settings(data)
-
-
-@api.post(endpoints["thresholds"])
-async def mod_settings(
-    cpu_percent: float = thresholds_dict["cpu_percent"],
-    ram_percent: float = thresholds_dict["ram_percent"],
-):
-    # if token:
-    #     blablabla
-    thresholds_dict["cpu_percent"] = cpu_percent
-    thresholds_dict["ram_percent"] = ram_percent
-    return {"thresholds": thresholds_dict}
+    msg = CONFIG.write_settings(data)
+    CONFIG = Settings()
+    return msg
 
 
 @api.on_event("startup")
-@repeat_every(seconds=config_file.post_interval, logger=logger, wait_first=True)
+@repeat_every(seconds=CONFIG.metrics.post_interval, logger=logger, wait_first=True)
 def periodic():
     # https://github.com/tiangolo/fastapi/issues/520
     # https://fastapi-utils.davidmontague.xyz/user-guide/repeated-tasks/#the-repeat_every-decorator
     # Changed Timeloop for this
     elapsed_time, data = send_metrics_adapter([static, dynamic])
-    try:
-        send_metrics(
-            url=config_file.post_metric_url,
-            elapsed_time=elapsed_time,
-            data=data,
-            file_enabled=config_file.metric_enable_file,
-            file_path=config_file.metric_file,
-        )
-    except (
-        ConnectionRefusedError,
-        requests.exceptions.ConnectionError,
-        NewConnectionError,
-        MaxRetryError,
-    ) as msg:
-        save2log(
-            type="ERROR",
-            data=f'Could not connect to "{config_file.post_metric_url}" ({msg})',
-        )
+    send_metrics(
+        url=CONFIG.metrics.post_url,
+        elapsed_time=elapsed_time,
+        data=data,
+        file_enabled=CONFIG.metrics.enable_logfile,
+        file_path=CONFIG.metrics.log_filename,
+    )
 
     alert = {}
     if data["cpu_percent"] >= thresholds_dict["cpu_percent"]:
@@ -114,36 +88,15 @@ def periodic():
     except KeyError as msg:
         pass
     if alert:
-        try:
-            r = requests.post(config_file.post_alert_url, json={"alert": alert})
-        except requests.exceptions.MissingSchema:
-            # If invalid URL is provided
-            save2log(
-                type="ERROR",
-                data=f'Invalid POST URL for Alerts ("{config_file.post_alert_url}")',
-            )
+        r = requests.post(CONFIG.alerts.url, json={"alert": alert})
 
 
 def start():
     """Launched with `poetry run start` at root level"""
-    uvicorn.run(
-        "monitor_agent.main:api",
-        host=config_file.host,
-        port=config_file.port,
-        reload=config_file.reload,
-        workers=config_file.workers,
-        log_level=config_file.log_level,
-        interface="asgi3",
-        debug=config_file.debug,
-        backlog=config_file.backlog,
-        timeout_keep_alive=config_file.timeout_keep_alive,
-        limit_concurrency=config_file.limit_concurrency,
-        limit_max_requests=config_file.limit_max_requests,
-        ssl_keyfile=config_file.ssl_keyfile,
-        ssl_keyfile_password=config_file.ssl_keyfile_password,
-        ssl_certfile=config_file.ssl_certfile,
-        ssl_version=config_file.ssl_version,
-        ssl_cert_reqs=config_file.ssl_cert_reqs,
-        ssl_ca_certs=config_file.ssl_ca_certs,
-        ssl_ciphers=config_file.ssl_ciphers,
-    )
+    uviconfig = {"app": "monitor_agent.main:api", "interface": "asgi3"}
+    uviconfig.update(CONFIG.uvicorn.__dict__)
+    uviconfig.pop("__module__", None)
+    uviconfig.pop("__dict__", None)
+    uviconfig.pop("__weakref__", None)
+    uviconfig.pop("__doc__", None)
+    uvicorn.run(**uviconfig)
--- monitor_agent/settings.py
+++ monitor_agent/settings.py
@@ -4,36 +4,6 @@ import json
 from monitor_agent.core.helper import save2log
 
 
-main_parameters = [
-    "host",
-    "port",
-    "workers",
-    "reload",
-    "debug",
-    "log_level",
-    "backlog",
-    "timeout_keep_alive",
-    "metric_endpoint",
-    "post_metric_url",
-    "post_interval",
-    "metric_enable_file",
-    "metric_file",
-    "post_alert_url",
-]
-
-optional_parameters = [
-    "ssl_keyfile",
-    "ssl_keyfile_password",
-    "ssl_certfile",
-    "ssl_version",
-    "ssl_cert_reqs",
-    "ssl_ca_certs",
-    "ssl_ciphers",
-    "limit_concurrency",
-    "limit_max_requests",
-]
-
-config_parameters = main_parameters + optional_parameters
 rel_path = "settings.json"
 dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
 abs_file_path = os.path.join(dir, rel_path)
@@ -41,15 +11,19 @@ abs_file_path = os.path.join(dir, rel_path)
 
 class Settings:
     def __init__(self):
-        for key, value in self._read_settings_file().items():
-            setattr(self, key, value)
+        self.as_dict: dict = self._read_settings_file()
+        obj = toObj(self.as_dict)
+        self.alerts = obj.alerts
+        self.metrics = obj.metrics
+        self.thresholds = obj.thresholds
+        self.uvicorn = obj.uvicorn
 
     def _read_settings_file(self):
         try:
             f = open(abs_file_path, "r")
             data = f.read()
-            data_dict = _validate_json(data)
-            data_str, data_dict = _format_json_file(data_dict, abs_file_path)
+            data_dict = json.loads(data)
+            data_str, data_dict = _write_file(data_dict, abs_file_path)
         except (json.JSONDecodeError, ValueError, FileNotFoundError) as msg:
             print(f"ERROR: Invalid JSON settings file - {msg}", file=sys.stderr)
             save2log(type="ERROR", data=f"Invalid JSON file - {msg}")
@@ -59,8 +33,8 @@ class Settings:
 
     def write_settings(self, data: str):
         try:
-            data_dict = _validate_json(data)
-            data_str, data_dict = _format_json_file(data_dict, abs_file_path)
+            data_dict = json.loads(data)
+            data_str, data_dict = _write_file(data_dict, abs_file_path)
         except (json.JSONDecodeError, ValueError) as msg:
             return {"status": f"Error: {msg}", "data": data}
 
@@ -73,36 +47,23 @@ class Settings:
 ######################
 
 
-def dict_keys_iterator(dictionary: dict):
-    for key, value in dictionary.items():
-        if isinstance(value, dict):
-            dict_keys_iterator(value)
-        yield key
+def toObj(item):
+    """Jakub Dóka: https://stackoverflow.com/a/65969444"""
+    if isinstance(item, dict):
+        obj = type("__object", (object,), {})
+
+        for key, value in item.items():
+            setattr(obj, key, toObj(value))
+
+        return obj
+    elif isinstance(item, list):
+        return map(toObj, item)
+    else:
+        return item
 
 
-def _validate_json(data: str):
-    data_dict = json.loads(data)
-
-    keys = [key for key in dict_keys_iterator(data_dict)]
-
-    diff_total = list(set(keys) - set(config_parameters))
-    if len(diff_total) > 0:
-        raise ValueError(f"Config file contains invalid parameters: {diff_total}")
-
-    diff_main = list(set(main_parameters) - set(keys))
-    if len(diff_main) > 0:
-        raise ValueError(f"Config file does not contain main parameters: {diff_main}")
-    return data_dict
-
-    # Need to validate type of data
-
-
-def _format_json_file(data: dict, path: str):
-    data_dict = data.copy()
-    for key in optional_parameters:
-        if key not in data_dict.keys():
-            data_dict[key] = None
+def _write_file(data: dict, path: str):
     f = open(path, "w")
-    data_str: str = json.dumps(data_dict, indent=4, sort_keys=True)
+    data_str: str = json.dumps(data, indent=4, sort_keys=True)
     f.write(data_str)
-    return data_str, data_dict
+    return data_str, data
