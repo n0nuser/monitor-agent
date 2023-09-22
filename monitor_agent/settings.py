623e8535076da52267a639787256d9089c4e2002
Pablo González <gonzrubio.pablo@gmail.com>
2023-09-22 18:26:00 +0200

    🎨 Improved Settings File

--- monitor_agent/settings.py
+++ monitor_agent/settings.py
@@ -1,100 +1,86 @@
-import os
 import json
-import logging
-import typing
+from pathlib import Path
+from uuid import UUID
+
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+directory = Path(__file__).resolve().parent
+abs_file_path = directory / "settings.json"
+if not abs_file_path.exists():
+    raise FileNotFoundError(f"File not found: {abs_file_path}")
 
 
-rel_path = "settings.json"
-directory = os.path.dirname(__file__)  # <-- absolute dir the script is in
-abs_file_path = os.path.join(directory, rel_path)
+class AlertsConfig(BaseModel):
+    """Alerts for the monitor_agent application."""
+
+    url: str
 
 
-class Settings:
-    def __init__(self):
-        self.as_dict: dict = self._read_settings_file()
-        _obj = toObj(self.as_dict)
-        self.alerts = _obj.alerts
-        self.auth = _obj.auth
-        self.logging = _obj.logging
-        self.metrics = _obj.metrics
-        self.endpoints = _obj.endpoints
-        self.thresholds = _obj.thresholds
-        self.uvicorn = _obj.uvicorn
+class AuthConfig(BaseModel):
+    """Auth for the monitor_agent application."""
 
-    def _read_settings_file(self) -> dict:
-        """Reads the settings file and returns the data as a dict.
-
-        Returns:
-            dict: Dictionary with the data from the settings file.
-        """
-        try:
-            f = open(abs_file_path, "r")
-            data_dict = json.loads(f.read())
-            _, data_dict = _write_to_json(data_dict, abs_file_path)
-        except (json.JSONDecodeError, ValueError, FileNotFoundError):
-            logging.error("Invalid JSON settings file.", exc_info=True)
-            exit()
-
-        return data_dict
-
-    def write_settings(self, data: str) -> dict:
-        """Writes the settings to the settings file.
-
-        Args:
-            data (str): The data to write to the settings file.
-
-        Returns:
-            dict: Dict with status of the write operation, and the data as a dictionary if successful.
-        """
-        try:
-            data_dict = json.loads(data)
-            _, data_dict = _write_to_json(data_dict, abs_file_path)
-        except (json.JSONDecodeError, ValueError) as msg:
-            return {"status": f"Error: {msg}", "data": data}
-
-        self.settings = data_dict
-        return {"status": "success", "data": data_dict}
+    agent_token: UUID
+    name: str
+    user_token: UUID
 
 
-####################
-# Helper functions #
-####################
+class EndpointsConfig(BaseModel):
+    """Endpoints for the monitor_agent application."""
+
+    agent_endpoint: str
+    metric_endpoint: str
 
 
-def toObj(item) -> object:
-    """Converts a dictionary to an object.
+class LoggingConfig(BaseModel):
+    """Logging for the monitor_agent application."""
 
-    Reference: Jakub Dóka: https://stackoverflow.com/a/65969444
-
-    Returns:
-        object: An object with the same data as the dictionary
-
-    """
-    if isinstance(item, dict):
-        obj = type("__object", (object,), {})
-
-        for key, value in item.items():
-            setattr(obj, key, toObj(value))
-
-        return obj
-    elif isinstance(item, list):
-        return map(toObj, item)
-    else:
-        return item
+    filename: str
+    level: str
 
 
+class MetricsConfig(BaseModel):
+    """Metric config for the monitor_agent application."""
 
-def _write_to_json(data: dict, path: str):
-    """Given a dictionary, write it to a json file with 4 space indentation.
+    enable_logfile: bool
+    get_endpoint: bool
+    log_filename: str
+    post_interval: int
 
-    Args:
-        data (dict): Dictionary to write to file
-        path (str): Path to the file
 
-    Returns:
-        (str, dict): Data written to the file; and the dictionary itself
-    """
-    f = open(path, "w")
-    data_str: str = json.dumps(data, indent=4, sort_keys=True)
-    f.write(data_str)
-    return data_str, data
+class ThresholdsConfig(BaseModel):
+    """Thresholds for the monitor_agent application."""
+
+    cpu_percent: int
+    ram_percent: int
+
+
+class UvicornConfig(BaseModel):
+    """Uvicorn configuration."""
+
+    backlog: int
+    debug: bool
+    host: str
+    log_level: str
+    port: int
+    reload: bool
+    timeout_keep_alive: int
+    workers: int
+
+
+class Settings(BaseSettings):
+    """Settings for the monitor_agent application."""
+
+    model_config = SettingsConfigDict(
+        env_file=(".env", ".env.prod"), env_file_encoding="utf-8"
+    )
+    alerts: AlertsConfig
+    auth: AuthConfig
+    endpoints: EndpointsConfig
+    logging: LoggingConfig
+    metrics: MetricsConfig
+    thresholds: ThresholdsConfig
+    uvicorn: UvicornConfig
+
+
+SETTINGS = Settings.model_validate_json(json.dumps(abs_file_path))
